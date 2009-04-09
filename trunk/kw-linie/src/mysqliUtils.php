15c42b49b017e368e1bf4af62f47694a290c7761
geert.van.bastelaere@gmail.com <geert.van.bastelaere@gmail.com@035ab5ea-1b8e-11de-a108-cd2f117ce590>
2009-04-09 20:51:47 +0000

    mySql utilities

---
+++ trunk/kw-linie/src/mysqliUtils.php
@@ -0,0 +1,53 @@
+<?php
+
+function newMysqli() {
+	$mysqli = new mysqli("localhost", "j_devit_be", "K3DyYQcn", "j_devit_be");
+	
+	return $mysqli;
+}
+
+function findSQL($sql) {
+	if ($mysqli = newMysqli()) {
+		if ($stmt = $mysqli->prepare($sql)) {
+			if ($stmt->execute()) {
+				$result = getresult($stmt);
+			}
+			$stmt->close();
+		}
+		$mysqli->close();
+	}
+	
+	return $result;
+}
+
+function getResult($stmt) {
+	$result = array();
+	 
+	$metadata = $stmt->result_metadata();
+	$fields = $metadata->fetch_fields();
+
+	for (;;) {
+		$pointers = array();
+		$row = new stdClass();
+		 
+		$pointers[] = $stmt;
+		foreach ($fields as $field) {
+			$fieldname = $field->name;
+			$pointers[] = &$row->$fieldname;
+		}
+		 
+		call_user_func_array(mysqli_stmt_bind_result, $pointers);
+		 
+		if (!$stmt->fetch()) {
+			break;
+		}
+		 
+		$result[] = $row;
+	}
+	 
+	$metadata->free();
+	 
+	return $result;
+}
+
+?>
