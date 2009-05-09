3a9e1b074d27919366f4fdfba137cc0a3433a48b
geert.van.bastelaere <geert.van.bastelaere@035ab5ea-1b8e-11de-a108-cd2f117ce590>
2009-05-09 18:00:23 +0000

    Fix mimetype

--- kw-linie/src/download_document.php
+++ kw-linie/src/download_document.php
@@ -10,7 +10,8 @@ if ($mysqli = newMysqli()) {
 	if ($stmt = $mysqli->prepare($sql)) {
 		$stmt->bind_param('i', $document_id);
 		if ($stmt->execute()) {
-			$result = getSingleResult($stmt);
+			$stmt->bind_result($filename, $mimetype, $size);
+			$result = $stmt->fetch();
 		}
 		$stmt->close();
 	}
@@ -28,13 +29,30 @@ if ($mysqli = newMysqli()) {
 	}
 	
 	$mysqli->close();
+	
+    $file_extension = strtolower(substr(strrchr($filename,"."),1));
+
+    switch ($file_extension) {
+        case "pdf": $ctype="application/pdf"; break;
+        case "exe": $ctype="application/octet-stream"; break;
+        case "zip": $ctype="application/zip"; break;
+        case "doc": $ctype="application/msword"; break;
+        case "xls": $ctype="application/vnd.ms-excel"; break;
+        case "ppt": $ctype="application/vnd.ms-powerpoint"; break;
+        case "gif": $ctype="image/gif"; break;
+        case "png": $ctype="image/png"; break;
+        case "jpe": case "jpeg":
+        case "jpg": $ctype="image/jpg"; break;
+        default: $ctype="application/force-download";
+    }
+
+	header("Content-type: $ctype");
+	header("Content-length: $size");
+	header("Content-Disposition: inline; filename=$filename");
+	header("Content-Description: PHP Generated Data");
+	
+	print $content;
+	
 }
-
-header("Content-type: $result->mimetype");
-header("Content-length: $result->size");
-header("Content-Disposition: attachment; filename=$result->filename");
-header("Content-Description: PHP Generated Data");
-
-print $content;
 
 ?>
