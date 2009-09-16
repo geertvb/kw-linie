00eb31ae817a9961db829f1742c59a99c66ada71
geert.van.bastelaere <geert.van.bastelaere@035ab5ea-1b8e-11de-a108-cd2f117ce590>
2009-09-16 16:54:05 +0000

    Security

--- trunk/kw-linie/src/joomla-user.php
+++ trunk/kw-linie/src/joomla-user.php
@@ -6,11 +6,28 @@ define('DS', DIRECTORY_SEPARATOR);
 
 require_once(JPATH_BASE . DS . 'includes' . DS . 'defines.php');
 require_once(JPATH_BASE . DS . 'includes' . DS . 'framework.php');
+require_once 'mysqliUtils.php';
 
 $mainframe = JFactory::getApplication('site');
 $mainframe->initialise();
 
 $user = JFactory::getUser();
+
+if (!$user->guest) {
+	$user->access = "public";
+	if ($mysqli = newMysqli()) {
+		$sql = "select `toegang` from `kwl_contact` where `gebruikersnaam`=? limit 1";
+		if ($stmt = $mysqli->prepare($sql)) {
+			$stmt->bind_param('i', $user->username);
+			if ($stmt->execute()) {
+				$row = getSingleResult($stmt);
+				$user->access = $row->toegang;
+			}
+			$stmt->close();
+		}
+		$mysqli->close();
+	}
+}
 
 $xml = new XMLWriter();
 $xml->openMemory();
@@ -24,6 +41,7 @@ if (!$user->guest) {
 	$xml->writeElement('name', $user->name);
 	$xml->writeElement('username', $user->username);
 	$xml->writeElement('usertype', $user->usertype);
+	$xml->writeElement('access', $user->access);
 }
 $xml->endElement();
 $xml->endDocument();
