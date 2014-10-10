d3dd6a50b513bcf89ec131686327c7fffd9bab36
geert.van.bastelaere@gmail.com <geert.van.bastelaere@gmail.com@035ab5ea-1b8e-11de-a108-cd2f117ce590>
2014-10-10 10:59:04 +0000

    kw-linie-html5

---
+++ app/views/hello.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en" ng-app>
+<head>
+	<meta charset="UTF-8">
+	<title>KW-Linie</title>
+    <script src="bower_components/angular/angular.js"></script>
+</head>
+<body>
+	<div class="welcome">
+        <p>Nothing here {{'yet' + '!'}}</p>
+	</div>
+</body>
+</html>
